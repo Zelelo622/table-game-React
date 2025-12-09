a20780b07a41140fa24c09ee1ca14b3abc86ac2b
Zelelo622 <egorcom27@gmail.com>
2025-12-09 20:33:18 +0300

    fix: upd config release

--- release.config.cjs
+++ release.config.cjs
@@ -3,21 +3,12 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
     // [
     //   "@semantic-release/changelog",
     //   {
     //     changelogFile: "CHANGELOG.md",
     //   },
     // ],
-    // "@semantic-release/npm",
-    // [
-    //   "@semantic-release/git",
-    //   {
-    //     assets: ["CHANGELOG.md", "package.json"],
-    //     message:
-    //       "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
-    //   },
-    // ],
-    "@semantic-release/github",
   ],
 };
