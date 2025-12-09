5fbbe8e0d617996a93ae168a5026e3ac719cc58a
Zelelo622 <egorcom27@gmail.com>
2025-12-09 20:30:54 +0300

    refactor: upd release

--- release.config.cjs
+++ release.config.cjs
@@ -3,21 +3,21 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
-      },
-    ],
+    // [
+    //   "@semantic-release/changelog",
+    //   {
+    //     changelogFile: "CHANGELOG.md",
+    //   },
+    // ],
+    // "@semantic-release/npm",
+    // [
+    //   "@semantic-release/git",
+    //   {
+    //     assets: ["CHANGELOG.md", "package.json"],
+    //     message:
+    //       "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
+    //   },
+    // ],
     "@semantic-release/github",
   ],
 };
